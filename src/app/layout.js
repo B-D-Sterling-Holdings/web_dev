0f066238097ac151e9d387f0dc726b30315eeb5a
Dhruv-Datta <dattadhruv930@gmail.com>
2025-06-01 23:14:28 -0500

    added title

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>B.D. Sterling Portfolio Analytics</title>
+        <title>B.D. Sterling</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
